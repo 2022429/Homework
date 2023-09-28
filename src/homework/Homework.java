24bb9ecf97f8308fb9515f0548ce0569ef1d5bbc
2022429 <146199262+2022429@users.noreply.github.com>
2023-09-28 19:00:20 +0100

    Initial commit

---
+++ src/homework/Homework.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package homework;
+
+/**
+ *
+ * @author sadiq
+ */
+public class Homework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World");
+        System.out.println("My name is Mohammed sadiq");
+        System.out.println("My student ID:2022429");
+        System.out.println("Iam from CCT college");
+    }
+    
+}
